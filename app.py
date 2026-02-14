06bd1df5bcd8c14a9182a67042453c5355d46ccc
CanevottiAlessia <153506270+CanevottiAlessia@users.noreply.github.com>
2026-02-14 12:25:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2567,10 +2567,10 @@ These exports are intended to support downstream analyses and custom pipelines.
     - In **Advanced options -> Tissue expression**, select tissues under **Expressed in (select tissues by system):** all cardiovascular-related tissues and fluids. 
 
     Under these conditions, 99 miRNAs are retained. For each entry, the app enables inspection of whether the locus:
-        - is conserved in mouse;
-        - displays expression across multiple cardiovascular tissues;
-        - is classified as structurally stable (R or D).
-    ---
+    - is conserved in mouse;
+    - displays expression across multiple cardiovascular tissues;
+    - is classified as structurally stable (R or D).
+    
     """,
         unsafe_allow_html=True
     )
@@ -2600,6 +2600,7 @@ These exports are intended to support downstream analyses and custom pipelines.
 
     st.markdown("---")
     st.markdown("License: CC BY 4.0")
+
 
 
 
